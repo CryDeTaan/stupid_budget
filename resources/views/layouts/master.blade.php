ec6d84a58a01de5a0ab65ec1c0b699d55ebe17f5
alicia.dewet <alicia.dewet@gmail.com>
2017-06-13 20:00:47 +0200

    added bulma to project - removed cdn

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Styles -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.min.css" rel="stylesheet">
+
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
