9df3395feca55eb7035a0d09e6c4ab82c5b7340a
alicia.dewet <alicia.dewet@gmail.com>
2017-06-17 07:23:23 +0200

    added content and images to landing page.

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -1,29 +1,17 @@
 <div class="columns">
     <div class="column">
-        <div class="card">
-            <header class="card-header has-text-centered">
-                <p class="card-header-title has-text-centered">
-                    Stupid Budget
-                </p>
-                <a class="card-header-icon">
-      <span class="icon">
-        <i class="fa fa-angle-down"></i>
-      </span>
-                </a>
-            </header>
-            <div class="card-content">
+        <div class="box">
+            <div class="media-content">
                 <div class="content">
-                    Have you heard of the term <em>Keep it simple stupid</em>?<br>
-                    Keep it simple stupid or K-I-S-S was used as a design principle by the US Navy in 60s'. This
-                    philosophy is what Stupid Budget strives too.
+
+                    <p class="header"> Stupid Budget </p>
+
+                    Have you ever heard of <em>Keep It Simple Stupid</em>?<br>
+                    Keep It Simple Stupid, or K-I-S-S, was developed and used as a design principle by the US Navy in the 60s. Stupid Budget is digital proof that Simple is Better.<br>
+                    Manage your money, simply, with Stupid Budget.
 
                 </div>
             </div>
-            <footer class="card-footer">
-                <a class="card-footer-item">Save</a>
-                <a class="card-footer-item">Edit</a>
-                <a class="card-footer-item">Delete</a>
-            </footer>
         </div>
     </div>
 </div>
@@ -33,8 +21,8 @@
     <div class="column">
         <div class="card">
             <div class="card-image">
-                <figure class="image is-4by3">
-                    <img src="images/accounts.png" alt="Image">
+                <figure class="image is-4by2">
+                    <img src="images/Accounts.png" alt="Image">
                 </figure>
             </div>
             <div class="card-content">
@@ -42,39 +30,29 @@
 
                     <div class="media-content">
                         <p class="title is-4">Accounts</p>
-                        {{--<p class="subtitle is-6">@johnsmith</p>--}}
                     </div>
                 </div>
 
                 <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                    <a>#css</a> <a>#responsive</a>
-                    <br>
-                    <small>11:09 PM - 1 Jan 2016</small>
+                    The <strong> Accounts </strong> page allows you to easily add access all of your banking or retail accounts on one view.
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-image">
-                <figure class="image is-4by3">
-                    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+                <figure class="image is-4by2">
+                    <img src="images/Expenses.png" alt="Image">
                 </figure>
             </div>
             <div class="card-content">
                 <div class="media">
                     <div class="media-content">
                         <p class="title is-4">Expenses</p>
-                        {{--<p class="subtitle is-6">@johnsmith</p>--}}
                     </div>
                 </div>
 
                 <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                    <a>#css</a> <a>#responsive</a>
-                    <br>
-                    <small>11:09 PM - 1 Jan 2016</small>
+                    The <strong> Expenses </strong> page helps you to track your spending and allows you to search for expenses by date.
                 </div>
             </div>
         </div>
@@ -82,32 +60,27 @@
     <div class="column">
         <div class="card">
             <div class="card-image">
-                <figure class="image is-4by3">
-                    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+                <figure class="image is-4by2">
+                    <img src="images/Income.png" alt="Image">
                 </figure>
             </div>
             <div class="card-content">
                 <div class="media">
 
                     <div class="media-content">
-                        <p class="title is-4">Expenses</p>
-                        {{--<p class="subtitle is-6">@johnsmith</p>--}}
+                        <p class="title is-4">Income</p>
                     </div>
                 </div>
 
                 <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                    <a>#css</a> <a>#responsive</a>
-                    <br>
-                    <small>11:09 PM - 1 Jan 2016</small>
+                    The <strong> Income </strong> page lets you add an income to your account(s) as you receive payment.
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-image">
-                <figure class="image is-4by3">
-                    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+                <figure class="image is-4by1">
+                    <img src="images/Budget.png" alt="Image">
                 </figure>
             </div>
             <div class="card-content">
@@ -115,48 +88,14 @@
 
                     <div class="media-content">
                         <p class="title is-4">Budget</p>
-                        {{--<p class="subtitle is-6">@johnsmith</p>--}}
                     </div>
                 </div>
 
                 <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                    <a>#css</a> <a>#responsive</a>
-                    <br>
-                    <small>11:09 PM - 1 Jan 2016</small>
+                    The (Stupid) <strong> Budget </strong> helps you to visually see your budget and spending with progress bars.
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-<div class="box">
-    <nav class="level">
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Tweets</p>
-                <p class="title">3,456</p>
-            </div>
-        </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Following</p>
-                <p class="title">123</p>
-            </div>
-        </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Followers</p>
-                <p class="title">456K</p>
-            </div>
-        </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Likes</p>
-                <p class="title">789</p>
-            </div>
-        </div>
-    </nav>
 </div>
 
