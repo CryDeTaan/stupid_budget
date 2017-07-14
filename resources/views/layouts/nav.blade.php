b44c739359a87aaa9961161527bb4a0dc93b7afe
Christiaan de Wet <christiaan.dewet@living-stratus.com>
2017-07-14 12:14:18 +0200

    Updated the categories route to be the budget route.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="tabs is-centered is-boxed">
     <ul>
         <router-link to="/accounts" tag="li"><a>Accounts</a></router-link>
-        <router-link to="/categories" tag="li"><a>Budget</a></router-link>
+        <router-link to="/budget" tag="li"><a>Budget</a></router-link>
         <router-link to="/income" tag="li"><a>Income</a></router-link>
         <router-link to="/expenses" tag="li"><a>Expenses</a></router-link>
     </ul>
