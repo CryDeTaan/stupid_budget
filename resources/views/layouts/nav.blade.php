3bac645fc816bb3a3a1f0ef4502b3e1fd0c8f730
CryDeTaan <crydetaan@gmail.com>
2017-08-02 19:40:48 +0200

    Fixed dashboard tab is selected after adding the feature again. :)

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <div class="tabs is-centered is-boxed">
     <ul>
-        <router-link to="/" tag="li"><a>Dashboard</a></router-link>
+        <router-link to="dashboard" tag="li"><a>Dashboard</a></router-link>
         <router-link to="budget" tag="li"><a>Budget</a></router-link>
         <router-link to="accounts" tag="li"><a>Accounts</a></router-link>
         <router-link to="income" tag="li"><a>Income</a></router-link>
