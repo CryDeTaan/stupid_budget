40f8c648ad860bfdaf921f783ed66acc396e6730
CryDeTaan <crydetaan@gmail.com>
2017-08-20 10:43:30 +0200

    Added GA to the vue routes. Awesome :)

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -4,14 +4,18 @@ import axios from 'axios';
 import Form from './utilities/Form';
 import moment from 'moment';
 import { gsap, TweenMax } from 'gsap';
+import ga from './ga';
 
 window.Vue = Vue;
 Vue.use(
-    VueRouter,
+    VueRouter
 );
-
 window.axios = axios;
 window.moment = moment;
+window.Form = Form;
+window.gsap = gsap;
+window.ga = ga;
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -21,6 +25,3 @@ window.moment = moment;
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.Form = Form;
-
-window.gsap = gsap;
